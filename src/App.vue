be65bb418408372589ba45fc94ea9a9bccedcb46
ysbsaji <ysbsaji999@gmail.com>
2021-08-29 10:30:43 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view />
+</template>
+<script>
+import { defineComponent } from 'vue';
+import { useQuasar } from "quasar";
+export default defineComponent({
+  setup() {
+    const $q = useQuasar()
+  },
+  name: 'App'
+})
+</script>
